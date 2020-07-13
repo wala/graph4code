4606b2ef5a7cf9332060c3e6a3e74e09dade2f83
Ibrahim Abdelaziz <i.hosny2@gmail.com>
2020-07-13 19:00:17 -0400

    working on graph loading scripts

--- scripts/load_graph4code.sh
+++ scripts/load_graph4code.sh
@@ -6,10 +6,15 @@
 #chmod +x *.sh
 
 echo "-------Downloading Graph4Code Files-----"
-SOURCE_DIR=$PWD
-QUADS_LOC=$SOURCE_DIR/graph4code_quads/
-JENA_DB_LOC=$SOURCE_DIR/graph4code_db/
-
+export SOURCE_DIR=$PWD
+export QUADS_LOC=$SOURCE_DIR/graph4code_quads/
+export JENA_DB_LOC=$SOURCE_DIR/graph4code_db/
+export JENA_LOC=$SOURCE_DIR/apache-jena-3.16.0
+export FUSEKI_LOC=$SOURCE_DIR/apache-jena-fuseki-3.16.0
+echo "SOURCE_DIR: $SOURCE_DIR"
+echo "JENA_LOC: $JENA_LOC"
+echo "QUADS_LOC: $QUADS_LOC"
+echo "JENA_DB_LOC: $JENA_DB_LOC"
 #-------Setup----------#
 echo "Main Directory: $source_dir"
 git clone https://github.com/wala/graph4code.git
@@ -20,17 +25,18 @@ tar -xzf apache-jena-fuseki-3.16.0.tar.gz
 
 wget http://mirror.metrocast.net/apache/jena/binaries/apache-jena-3.16.0.tar.gz
 tar -xzf apache-jena-3.16.0.tar.gz
-JENA_LOC=$SOURCE_DIR/apache-jena-fuseki-3.16.0
-FUSEKI_LOC=$SOURCE_DIR/apache-jena-fuseki-3.16.0
 #-------END OF Setup----------#
 
 cd $SOURCE_DIR/graph4code/scripts
+chmod +x *.sh
 ./download_graph.sh $QUADS_LOC
 
 
 echo "-------Build Graph4Code JENA DB-----"
+cd $SOURCE_DIR/graph4code/scripts
 ./build_graph.sh $JENA_LOC $QUADS_LOC $JENA_DB_LOC
 
 echo "-------Launch FUSEKI over Graph4Code JENA DB-----"
+cd $SOURCE_DIR/graph4code/scripts
 ./serve_graph.sh $FUSEKI_LOC $JENA_DB_LOC
 
