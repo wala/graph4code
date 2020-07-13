a61e237f74b55f7b7adae49d64eaf8baf5d5bac9
Ibrahim Abdelaziz <i.hosny2@gmail.com>
2020-07-13 16:41:26 -0400

    working on graph loading scripts

--- scripts/load_graph4code.sh
+++ scripts/load_graph4code.sh
@@ -1,4 +1,4 @@
-java -Xmx100g -jar fuseki-server.jar --port 3030 --tdb2 --loc $JENA_DB_LOC /graph_v1_0 >> logs/serve_graph_g1_v1_0.log 2>&1 
+#!/bin/bash
 
 echo "-------Cloning Graph4Code Github Repo-----"
 git clone https://github.com/wala/graph4code.git
